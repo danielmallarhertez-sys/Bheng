876ae55783ddd7cbcd9bc92b34368ffa5083bdc4
danielmallarhertez-sys <danielmallarhertez@gmail.com>
2026-07-22 16:28:38 +0800

    Update celebrate.js

--- js/celebrate.js
+++ js/celebrate.js
@@ -361,9 +361,8 @@ const videoPlaylist = [
     "videos/video3.mp4",
     "videos/video4.mp4",
     "videos/video5.mp4",
-    "videos/video6.mp4"
-    // Add more if you have them:
-    // "videos/video7.mp4",
+    "videos/video6.mp4",
+    "videos/video7.mp4"
     // "videos/video8.mp4",
     // "videos/video9.mp4",
     // "videos/video10.mp4"
